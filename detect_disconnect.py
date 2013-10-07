9ea0354ca2532618ef6848a84ea610646f45bd1a
Kyu Lee <smailq@gmail.com>
2013-10-07 16:43:16 -0400

    Added extra process to monitor expired items

--- detect_disconnect.py
+++ detect_disconnect.py
@@ -1,15 +1,39 @@
 # The core API class
 from cosmic.api import API
 # We use teleport to represent types
-from teleport import Integer, String, Struct, required, optional
+from teleport import Integer, String, Struct, required
 
 import os
 from datetime import datetime, timedelta
+import time
+
+from multiprocessing import Process, Manager
+
+
+def make_request(monitoring_items):
+    while True:
+        time.sleep(0.5)
+
+        if len(monitoring_items) <= 0:
+            continue
+
+        urls = []
+
+        for token, value in monitoring_items.items():
+            if value[1] < datetime.now():
+                urls.append((token, value[0]))
+
+        for token, _ in urls:
+            del monitoring_items[token]
+
+        if len(urls) > 0:
+            print urls
 
 # Name your API
 api = API("detect_disconnect")
 
-monitoring_items = {}
+manager = Manager()
+monitoring_items = manager.dict()
 
 # Decorate your function with the parameter type and return type
 @api.action(
@@ -30,21 +54,27 @@ def monitor(token, timeout, url):
   The token will be cleared after the GET request is triggered.
 
   Max timeout is 60 seconds.
+
+  Timeout of 0 will remove the item.
   """
 
-  if timeout < 1 or timeout > 60:
-    raise Exception("Timeout must be between [1 - 60] ")
+  if timeout > 60:
+    raise Exception("Timeout must be between [0 - 60]")
 
   if token in monitoring_items:
     # Make sure URL is identical
     if monitoring_items[token][0] != url:
         raise Exception("Token is already in use.")
 
-  # Update time with timeout setting.
-  monitoring_items[token] = (url, datetime.now() + timedelta(seconds=timeout))
+  if timeout == 0 and token in monitoring_items:
+    del monitoring_items[token]
+  elif timeout > 0:
+    # Update time with timeout setting.
+    monitoring_items[token] = (url, datetime.now() + timedelta(seconds=timeout))
 
-  print (token, timeout, url)
-  
+
 # Start running the api
 if __name__ == "__main__":
-  api.run(port=os.environ.get('PORT', 5100), debug=True)
+    p = Process(target=make_request, args=(monitoring_items,))
+    p.start()
+    api.run(port=os.environ.get('PORT', 5100), debug=False)
